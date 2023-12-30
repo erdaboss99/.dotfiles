5d1fb0d63cd4481b166ea54784974092d4812b54
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-12-30 01:51:24 +0100

    Init repository

---
+++ README.md
@@ -0,0 +1,65 @@
+# Steps to bootstrap a new Mac
+
+1. Install Apple's Command Line Tools
+
+```zsh
+xcode-select --install
+```
+
+2. Install Brew
+
+[Homebrew website](https://brew.sh/)
+
+3. Clone the repo
+
+```zsh
+# Use SSH (if set up)...
+git clone git@github.com:erdaboss99/.dotfiles.git ~/.dotfiles
+```
+
+```zsh
+# ...or use HTTPS and switch remotes later.
+git clone https://github.com/erdaboss99/.dotfiles.git ~/.dotfiles
+```
+
+4. Install Homebrew packages from `Brewfile`.
+
+```zsh
+brew bundle
+```
+
+5. Create symlinks
+
+```zsh
+# zsh config
+ln -s ~/.dotfiles/root/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/root/.zprofile ~/.zprofile
+```
+
+```zsh
+# fzf ignore
+ln -s ~/.dotfiles/root/.ignore ~/.ignore
+```
+
+```zsh
+# TMUX config
+# TPM is required! Install it from here [TMUX Plugin Manager](https://github.com/tmux-plugins/tpm)
+ln -s ~/.dotfiles/root/.tmux.conf ~/.tmux.conf
+```
+
+```zsh
+# BACKUP ONLY
+# NVChad custom config folder
+# NVChad is required! Install it from here [NVChad](https://nvchad.com/docs/quickstart/install)
+ln -s ~/.dotfiles/nvchad/custom ~/.config/nvim/lua/custom
+```
+
+```zsh
+# NVIM config
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+```
+
+```zsh
+# wezterm config
+ln -s ~/.dotfiles/root/.wezterm.lua ~/.wezterm.lua
+```
---
+++ root/.wezterm.lua
@@ -0,0 +1,136 @@
+local wezterm = require "wezterm"
+
+-- -- https://github.com/wez/wezterm/issues/3731#issuecomment-1592198263
+-- local function is_vim(pane)
+-- 	local is_vim_env = pane:get_user_vars().IS_NVIM == "true"
+-- 	if is_vim_env == true then return true end
+-- 	local process_name = string.gsub(pane:get_foreground_process_name(), "(.*[/\\])(.*)", "%2")
+-- 	return process_name == "nvim" or process_name == "vim"
+-- end
+
+local super_vim_keys_map = {
+	s = utf8.char(0xAA), -- <CMD-s>
+	b = utf8.char(0xAB), -- <CMD-b>
+}
+
+local function bind_super_key_to_vim(key)
+	return {
+		key = key,
+		mods = "CMD",
+		action = wezterm.action_callback(function(win, pane)
+			local char = super_vim_keys_map[key]
+			if char then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = {
+						key = char,
+						mods = nil,
+					},
+				}, pane)
+			else
+				win:perform_action({
+					SendKey = {
+						key = key,
+						mods = "CMD",
+					},
+				}, pane)
+			end
+		end),
+	}
+end
+
+return {
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = false,
+	font_size = 20.0,
+	font = wezterm.font_with_fallback {
+		{
+			family = "JetBrains Mono",
+		},
+		{
+			family = "JetBrainsMono Nerd Font",
+		},
+	},
+	font_rules = {
+		{
+			intensity = "Bold",
+			italic = false,
+			font = wezterm.font_with_fallback {
+				{
+					family = "JetBrains Mono",
+					weight = "Bold",
+				},
+				{
+					family = "JetBrainsMono Nerd Font",
+					weight = "Bold",
+				},
+			},
+		},
+		{
+			intensity = "Normal",
+			italic = true,
+			font = wezterm.font_with_fallback {
+				{
+					family = "JetBrains Mono",
+					italic = true,
+				},
+				{
+					family = "JetBrainsMono Nerd Font",
+					italic = true,
+				},
+			},
+		},
+		{
+			intensity = "Bold",
+			italic = true,
+			font = wezterm.font_with_fallback {
+				{
+					family = "JetBrains Mono",
+					italic = true,
+					weight = "Bold",
+				},
+				{
+					family = "JetBrainsMono Nerd Font",
+					italic = true,
+					weight = "Bold",
+				},
+			},
+		},
+	},
+	use_cap_height_to_scale_fallback_fonts = true,
+
+	macos_window_background_blur = 20,
+	window_background_opacity = 0.9,
+	window_decorations = "RESIZE",
+	window_padding = {
+		left = 1,
+		right = 1,
+		top = 1,
+		bottom = 1,
+	},
+	exit_behavior = "CloseOnCleanExit",
+	window_close_confirmation = "NeverPrompt",
+
+	mouse_bindings = {
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = {
+				Up = {
+					streak = 1,
+					button = "Left",
+				},
+			},
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
+	send_composed_key_when_left_alt_is_pressed = false,
+	send_composed_key_when_right_alt_is_pressed = false,
+	use_dead_keys = false,
+
+	keys = {
+		bind_super_key_to_vim "s",
+		bind_super_key_to_vim "b",
+		{ key = "/", mods = "CTRL", action = wezterm.action { SendString = "\x1f" } },
+	},
+}
